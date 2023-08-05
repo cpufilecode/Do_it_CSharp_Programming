6483f95a224a69524d492309aaead5d3eb6ebd7a
cpufilecode <zf0908@naver.com>
2023-08-05 13:41:26 +0900

    upload

--- Variable/Source03_02.cs
+++ Variable/Source03_02.cs
@@ -5,7 +5,12 @@ class MainClass
     public static void Main(string[] args)
     {
         int num = 0;
-        
 
+        if (num > 0)
+            Console.WriteLine("양수");
+        else if (num < 0)
+            Console.WriteLine("음수");
+        else
+            Console.WriteLine("영");
     }
 }
