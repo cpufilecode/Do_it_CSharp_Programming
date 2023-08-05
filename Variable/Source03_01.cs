7fd90531decf35b30f410d80853370773c083d8a
cpufilecode <zf0908@naver.com>
2023-08-05 13:50:19 +0900

    upload

---
+++ Variable/Source03_01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        bool boolVariable = true;
+        int intVarable = 10;
+        float floatVarable = 3.4F;
+        char charVariable = 'a';
+
+        Console.WriteLine(boolVariable);
+        Console.WriteLine(intVarable);
+        Console.WriteLine(floatVarable);
+        Console.WriteLine(charVariable);
+    }
+}
