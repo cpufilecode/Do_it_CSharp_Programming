e4c1d63119011a05bca7c6a0a9de7e3f9c8f4a42
cpufilecode <zf0908@naver.com>
2023-08-05 13:24:24 +0900

    upload

--- Variable/Program.cs
+++ Variable/Program.cs
@@ -4,6 +4,11 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        bool boolVariable = true;
+        int intVarable = 10;
+        float floatVarable = 3.4F;
+        char charVariable = 'a'; 
+
 
     }
 }
