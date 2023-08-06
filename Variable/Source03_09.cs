a8833e1ebd2e231eadd6b1a2c9367698b0418296
cpufilecode <zf0908@naver.com>
2023-08-06 20:30:43 +0900

    upload

--- Variable/Source03_09.cs
+++ Variable/Source03_09.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Net.Sockets;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
+        int score = 85;
 
+        if (score > 80)
+            if (score <= 100)
+                Console.WriteLine("A");
     }
 }
