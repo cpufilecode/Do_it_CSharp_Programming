40e3061e9f286adba3c3a28ce32be06561f263e2
cpufilecode <zf0908@naver.com>
2023-08-06 19:26:49 +0900

    upload

--- Variable/Source03_06.cs
+++ Variable/Source03_06.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Variable
+class MainClass
 {
-    internal class Source03_06
-    {
-    }
+
 }
