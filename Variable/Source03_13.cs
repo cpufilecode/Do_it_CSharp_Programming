84615fb7fa36f5cbd79805437113f63f3aa26378
cpufilecode <zf0908@naver.com>
2023-08-06 21:00:37 +0900

    upload

--- Variable/Source03_13.cs
+++ Variable/Source03_13.cs
@@ -6,7 +6,7 @@ class MainClass
     {
         sbyte value1 = 64;
         sbyte value2 = 64;
-        int value3 = value1 + value2;
+        int value3 = (int)value1 + value2;
         Console.WriteLine(value3);
     }
 }
