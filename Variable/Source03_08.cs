1bdbea98e0e32e445220fc3e8f9e7c91bf30188f
cpufilecode <zf0908@naver.com>
2023-08-06 20:21:24 +0900

    upload

--- Variable/Source03_08.cs
+++ Variable/Source03_08.cs
@@ -4,6 +4,12 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        bool A, B;
 
+        A = true;
+        B = false;
+
+        Console.WriteLine(A && A);
+        Console.WriteLine(A && B);
     }
 }
