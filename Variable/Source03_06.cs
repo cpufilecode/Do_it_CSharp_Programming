fc0e0860f1f37499bf882def6b69d0d7afc3a1e4
cpufilecode <zf0908@naver.com>
2023-08-06 19:37:03 +0900

    upload

--- Variable/Source03_06.cs
+++ Variable/Source03_06.cs
@@ -2,5 +2,12 @@
 
 class MainClass
 {
-
+    public static void Main(string[] args)
+    {
+        int num = 0;
+        Console.WriteLine(num++);
+        Console.WriteLine(num);
+        Console.WriteLine(--num);
+        Console.WriteLine(num);
+    }
 }
