a96e4375c61983e5f927c909f21f68f730329d8e
cpufilecode <zf0908@naver.com>
2023-09-16 21:07:21 +0900

    upload

---
+++ 04/04_01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Person
+{
+    public string Name = null;
+    public string Bithday = null;
+    public string a = null;
+
+    public void eat()
+    {
+        Console.WriteLine(Name + "이(가) 아침을 먹었습니다.");
+    }
+    
+    public void walk()
+    {
+        Console.WriteLine(Name + "이(가) 걷습니다.");
+    }
+
+    public void run()
+    {
+        Console.WriteLine(Name + "이(가) 뜁니다.");
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Person p1;
+        p1 = new Person();
+        p1.Name = "서준";
+        p1.eat();
+    }
+}
