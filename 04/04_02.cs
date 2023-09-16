e023991a4277275fa28c5c46f7728c21f3e72ba7
cpufilecode <zf0908@naver.com>
2023-09-16 21:25:19 +0900

    upload

---
+++ 04/04_02.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Cat
+{
+    public string Name = null;
+
+    public Cat()
+    {
+        Console.WriteLine("생성자가 호출되었습니다.");
+    }
+}
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Cat myCat = new Cat();
+    }
+}
---
+++ 04/04_04.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Cat
+{
+    public string Name = null;
+    public int Weight = 0;
+
+    public Cat(string name)
+    {
+        Name = name;
+        Console.WriteLine("고양이의 이름은 "+ Name +"입니다. ");
+    }
+}
