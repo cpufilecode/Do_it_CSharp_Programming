c17f84b046762181d4e1b039cc00d063002bde7e
cpufilecode <zf0908@naver.com>
2023-10-09 22:29:56 +0900

    upload

---
+++ 05/05_10.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Hashtable ht = new Hashtable();
+
+        ht["apple"] = "사과";
+        ht["banana"] = "바나나";
+        ht["orange"] = "오렌지";
+
+        Console.WriteLine(ht["apple"]);
+        Console.WriteLine(ht["banana"]);
+        Console.WriteLine(ht["orange"]);
+    }
+}
