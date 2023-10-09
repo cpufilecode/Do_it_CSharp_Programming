ccb7b864640b1a571452d30c04da385409664273
cpufilecode <zf0908@naver.com>
2023-10-09 22:16:02 +0900

    upload

---
+++ 05/05_08.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Queue qu = new Queue();
+
+        qu.Enqueue(1);
+        qu.Enqueue(2);
+        qu.Enqueue(3);
+
+        while (qu.Count > 0)
+        {
+            Console.WriteLine(qu.Dequeue());
+        }
+    }
+}
