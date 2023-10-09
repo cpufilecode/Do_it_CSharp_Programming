c20b3b0dde0aaa96decffd86072ddefbc515fb21
cpufilecode <zf0908@naver.com>
2023-10-09 21:50:08 +0900

    upload

--- 05/05_07.cs
+++ 05/05_07.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace _05
+class MainClass
 {
-    internal class _05_07
+    public static void Main(string[] args)
     {
+
     }
 }
