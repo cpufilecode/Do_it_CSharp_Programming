8d9cb0a713e9ed79b3ce09500a26625042a48415
cpufilecode <zf0908@naver.com>
2023-10-09 22:16:06 +0900

    upload

---
+++ 05/05_09.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Stack st = new Stack();
+
+        st.Push(1);
+        st.Push(2);
+        st.Push(3);
+
+        while (st.Count > 0)
+        {
+            Console.WriteLine(st.Pop());
+        }
+    }
+}
