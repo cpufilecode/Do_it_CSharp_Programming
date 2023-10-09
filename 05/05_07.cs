965badc7d997f2748577843e228f6bdc9bdcc6bc
cpufilecode <zf0908@naver.com>
2023-10-09 21:53:39 +0900

    upload

--- 05/05_07.cs
+++ 05/05_07.cs
@@ -5,6 +5,19 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        ArrayList al =  new ArrayList();
+
+        al.Add(1);
+        al.Add("Hello");
+        al.Add(3.3);
+        al.Add(true);
+
+        foreach(var item in al)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine();
+
 
     }
 }
