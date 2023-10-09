ca164ba9c069bd92f399c0feb74129368284546e
cpufilecode <zf0908@naver.com>
2023-10-09 14:53:08 +0900

    upload

---
+++ 05/05_01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[] array1 = new int[3];
+        array1[0] = 10;
+        array1[1] = 20;
+        array1[2] = 30;
+
+        int[] array2 = new int[] { 1, 2, 3 };
+
+        int[] array3 = new int[] { 4, 5, 6 };
+
+        Console.WriteLine(array1);
+        Console.WriteLine(array2);
+        Console.WriteLine(array3);
+    }
+}
